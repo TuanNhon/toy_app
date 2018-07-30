b48a6a9a511c0cf1fdf97469514154d3e141adec
Nguyen Duc Tuan Anh <tuananhnd2112@gmail.com>
2018-07-30 13:12:06 +0700

    sign-up

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,8 @@
 module UsersHelper
+  def gravatar_for user, size: Settings.user.gravatar_size
+    gravatar_id = Digest::MD5::hexdigest user.email.downcase
+    gravatar_url = Settings.user.gravatar_url % [gravatar_id: gravatar_id,
+      gravatar_size: size]
+    image_tag gravatar_url, alt: user.name, class: "gravatar"
+  end
 end

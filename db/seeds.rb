563d328c91a716619b980b576408cbd3ec892b4f
Nguyen Duc Tuan Anh <nhongbay2112bt@gmail.com>
2018-08-07 17:04:22 +0700

    User microposts part II

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,10 +2,16 @@ User.create! name:  "Example User", email: "example@railstutorial.org",
   password: "111111", password_confirmation: "111111", admin: true,
   activated: true, activated_at: Time.zone.now
 
-99.times do |n|
+10.times do |n|
   name  = Faker::Name.name
   email = "example-#{n+1}@railstutorial.org"
   password = "111111"
   User.create! name:  name, email: email, password: password, activated: true,
     password_confirmation: password, activated_at: Time.zone.now
+end
+
+users = User.order(:created_at).take(6)
+20.times do
+  content = Faker::Lorem.sentence(5)
+  users.each {|user| user.microposts.create!(content: content)}
 end
